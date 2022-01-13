f7709fd0721d2960845f17f69dc1a690bfe40a24
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2022-01-14 01:43:40 +0200

    added tag_page function

--- server.py
+++ server.py
@@ -309,7 +309,7 @@ def add_tag_page(question_id):
 @app.route("/question/<question_id>/tag/<tag_id>/delete-tag")
 def delete_tag_page(question_id, tag_id):
     database_manager.delete_tag_relation(question_id, tag_id)
-    return redirect(url_for("list_page"))
+    return redirect(url_for("question_page", question_id=question_id))
 
 
 @app.route("/sort_by")
